a89202731d281e571af584383abe9b1fd2d65092
rex <ling,zhang9@hp.com>
2016-06-27 14:35:54 +0800

    minor optimize.

--- src/main/java/com/quartzx/datacollector/utility/ResourceManager.java
+++ src/main/java/com/quartzx/datacollector/utility/ResourceManager.java
@@ -4,6 +4,7 @@ import com.quartzx.datacollector.CommonException;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by zling on 6/27/2016.
@@ -11,9 +12,12 @@ import java.io.IOException;
 public class ResourceManager {
     public String read(String fileName) {
         ClassLoader loader = getClass().getClassLoader();
+        InputStream stream = loader.getResourceAsStream(fileName);
+        if(stream == null)
+            throw new CommonException(String.format("%s not found." , fileName));
+
         try {
-            String result = IOUtils.toString(loader.getResourceAsStream(fileName));
-            return result;
+            return IOUtils.toString(stream);
         } catch (IOException ex){
             throw new CommonException("Read resource fail.", ex);
         }
